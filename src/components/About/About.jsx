6f85c34ba8886e70913d85b99935d05bf900e8e2
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-04-14 12:22:27 +0600

    added 3 person in the about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,44 +2,67 @@ import React, { useState } from 'react';
 import lavina from '../../assets/Lavina Photo.jpg'
 import tanvir from '../../assets/Tanvir Headshot.jpg'
 import atul from '../../assets/atul gupta_photo.jpeg'
+import ayesha from '../../assets/ayeshaayubi.jpg'
+import anita from '../../assets/anita.jpg'
+import nick from '../../assets/nick.jpg'
 import Modal from '../Modal/Modal';
 const About = () => {
     const [name, setName] = useState('')
     const [modalStatus, setModalStatus] = useState(false)
+    const users = [
+        {
+            name: "Lavina Samtani",
+            position: "Member Relations",
+            img: lavina
+        },
+        {
+            name: "Tanvir Aziz",
+            position: "President",
+            img: tanvir
+        },
+        {
+            name: "Atul Gulta",
+            position: "Treasurer",
+            img: atul
+        },
+        {
+            name: "Ayesha Ayubi",
+            position: "Director - Business Relations",
+            img: ayesha
+        },
+        {
+            name: "Anita George",
+            position: "Cultural Director",
+            img: anita
+        },
+        {
+            name: "Nick Rowan",
+            position: "Education Director",
+            img: nick
+        },
+
+    ]
     return (
         <div>
             <h1 className='text-center text-black text-2xl font-bold my-4 uppercase'>About Us</h1>
             <div id='about' className='my-4 p-4 grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-                <div className='rounded shadow-lg p-4 text-center'>
-                    <img src={lavina} alt="" className='rounded-full mb-3 h-[220px] w-[220px] mx-auto' />
-                    <div>
-                        <h3 className='font-bold capitalize text-gray-500'>Lavina Samtani</h3>
-                        <h4 className='font-bold capitalize text-gray-500'>Member Relations</h4>
-                        <button onClick={() =>setName('Lavina Samtani')}  className='my-4'><label onClick={() => setModalStatus(true)} htmlFor="see-bio-modal" className="px-6 py-2 rounded text-white uppercase text-sm bg-gray-800 hover:bg-gray-900 font-bold cursor-pointer">view bio</label></button>
-                    </div>
-                </div>
-                <div className='rounded shadow-lg p-4 text-center'>
-                    <img src={tanvir} alt="" className='rounded-full mb-3 h-[220px] w-[220px] mx-auto' />
-                    <div>
-                        <h3 className='font-bold capitalize text-gray-500'>Tanvir Aziz</h3>
-                        <h4 className='font-bold capitalize text-gray-500'>President</h4>
-                        <button onClick={() =>setName('Tanvir Aziz')} className='my-4'><label onClick={() => setModalStatus(true)} htmlFor="see-bio-modal" className="px-6 py-2 rounded text-white uppercase text-sm bg-gray-800 hover:bg-gray-900 font-bold cursor-pointer">view bio</label></button>
-                    </div>
-                </div>
-                <div className='rounded shadow-lg p-4 text-center'>
-                    <img src={atul} alt="" className='rounded-full mb-3 h-[220px] w-[220px] mx-auto' />
-                    <div>
-                        <h3 className='font-bold capitalize text-gray-500'>Atul Gulta</h3>
-                        <h4 className='font-bold capitalize text-gray-500'>Treasurer</h4>
-                        <button onClick={() =>setName('Atul Gulta')} className='my-4'><label onClick={() => setModalStatus(true)} htmlFor="see-bio-modal" className="px-6 py-2 rounded text-white uppercase text-sm bg-gray-800 hover:bg-gray-900 font-bold cursor-pointer">view bio</label></button>
-                    </div>
-
-                </div>
+                {
+                    users.map((user, index) => {
+                        return <div key={index} className='rounded shadow-lg p-4 text-center'>
+                            <img src={user.img} alt="" className='rounded-full mb-3 h-[220px] w-[220px] mx-auto' />
+                            <div>
+                                <h3 className='font-bold capitalize text-gray-500'>{user.name}</h3>
+                                <h4 className='font-bold capitalize text-gray-500'>{name.position}</h4>
+                                <button onClick={() => setName(`${user.name}`)} className='my-4'><label onClick={() => setModalStatus(true)} htmlFor="see-bio-modal" className="px-6 py-2 rounded text-white uppercase text-sm bg-gray-800 hover:bg-gray-900 font-bold cursor-pointer">view bio</label></button>
+                            </div>
+                        </div>
+                    })
+                }
             </div>
             {
-                name.length>0 && <Modal names = {{name, modalStatus, setModalStatus}}></Modal>
+                name.length > 0 && <Modal names={{ name, modalStatus, setModalStatus }}></Modal>
             }
-           
+
         </div>
 
     );
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,7 +3,6 @@ import './Modal.css'
 const Modal = ({ names }) => {
     const [person, setPerson] = useState("")
     const { name, modalStatus, setModalStatus } = names
-   
     const peoples = [
         {
             name: "Lavina Samtani",
@@ -16,8 +15,8 @@ const Modal = ({ names }) => {
         {
             name: "Tanvir Aziz",
             title: "President",
-            profession: '',
-            company: '',
+            profession: 'Founder',
+            company: 'AtoZ Title and Settlement',
             role: '',
             about: "SAAREP – an idea started with the creation of a professional networking platform for South Asian Real Estate Professionals in DC, MD and VA. Slowly we evolved in last few months, and it was incredible to find out that to reach our objective to have an Informed Home Ownership, it requires support and engagement that goes beyond just having a platform for professionals who are directly involved with Real Estate. Now SAAREP approach has changed to – “Anyone contributing to the Real Estate industry, directly and indirectly”. Such objective aims to serve anyone who needs access to a platform that aims to contribute to improving lives for the entire community. How real estate is intertwined with everything that we do in our daily lives, requires our rigorous involvement to create leadership in the community, to strengthen access to professional knowledge base and especially have a vision toward wealth building for all. I spanly believe that our unconditional dedication and support for each other’s growth is already happening withing the community. Having a network like SAAREP, it creates more opportunities for everyone to leverage growing personally and professionally which will lead to an “Informed Home Ownership” journey for the entire community, and beyond."
         },
@@ -28,6 +27,30 @@ const Modal = ({ names }) => {
             company: '',
             role: '',
             about: "As an owner of DMV Home Funding and with more than 18 years’ experience in the mortgage industry my mission statement is quite simple: service, service, service. My goal is to build long-term partnerships with immigrant first-time home buyers and immigrant realtors. No matter how challenging the situation, I will gladly take it on. I am highly responsive, and will complete the process to everyone’s satisfaction. After all, when the loan is approved and closing takes place, everyone wins, the realtors, the mortgage company, the loan officer, and the home buyers. Win-win-win-win! Why I Joined SAREP-   As per my mission I always wanted to help immigrant borrowers and I think SAREP is the best platform to join and help like minded people and eventually help our community."
+        },
+        {
+            name: "Ayesha Ayubi",
+            title: "Director - Business Relations",
+            profession: '',
+            company: '',
+            role: '',
+            about: "As a Realtor with Berkshire Hathaway HomeServices PenFed Realty, When I think of SAAREP it gives me the sense of belonging, a platform that promotes the highest degree of professional ability in the real estate industry,through shared support and care it fosters a sense of unity among members and create a forum for all real estate professionals."
+        },
+        {
+            name: "Anita George",
+            title: "Cultural Director",
+            profession: 'Profession Lender',
+            company: 'Company Titan Lending',
+            role: 'Role Sr loan Officer and Business Development',
+            about: "SAAREP, which stands for South Asian Association of Real Estate Professionals, is a fantastic platform designed to provide networking, collaboration, education, consultation, referrals, and cultural events for professionals in the real estate industry. The organization serves as a hub for connecting like-minded individuals within South Asian communities, bringing together individuals from diverse backgrounds and cultures to exchange ideas and best practices. Through their numerous events, they offer valuable insights on the latest trends and developments in the industry, as well as opportunities to learn from experts and engage in meaningful discussions on pressing issues facing the industry. By providing access to resources and fostering collaboration, SAAREP plays a key role in helping real estate professionals succeed and grow their businesses. One of the key goals of SAAREP is to support and empower real estate professionals like yourself, who are looking to make a difference in their communities and grow their businesses. By joining this association, you'll have access to valuable resources, such as mentorship opportunities, educational programs, and networking events, that can help you enhance your skills and knowledge, expand your professional network, and find new career opportunities. Additionally, you'll be able to connect with other professionals who share your vision and passion for making positive changes in the industry, and collaborate on projects and initiatives that can benefit your clients and the community as a whole. Whether you're just starting out or looking to take your career to the next level, SAAREP is an excellent platform that can provide you with the support and guidance you need to achieve your goals. When I started in the lending sector 22 years ago, there was many a times I wished I had the support of my community.I am a long term resident of the DMV area and have two kids and a dog.I am a foodie and love to try new cuisines.In my free time, I also love to travel and experience different cultures and cuisines."
+        },
+        {
+            name: "Nick Rowan",
+            title: "Education Director",
+            profession: 'Real Estate Consultant',
+            company: 'Trademark Realty',
+            role: '',
+            about: "An educated consumer will make educated decisions, and this is particularly crucial to successful Real Estate transactions. An educated consumer has the best chance of avoiding the many pitfalls which might be encountered in today’s Real Estate market.Setting goals – and working collectively with others to achieve those goals – is a byproduct of my South Asian heritage. At an early age, it became evident to me that articulating goals to others to form a cohesive team working toward these goals was the surest way to achieve success.To many, owning your own home is a clear sign of success. It is with this in mind that my job becomes clear. My job is to help others achieve their Real Estate aspirations. Helping someone be successful in their Real Estate journey is my mission. I joined SAAREP because I fully understand and believe in it’s mission of helping to connect the Real Estate community to one another and ultimately build dreams and strengthening the South Asian community. Together we will do great things."
         }
     ]
     useEffect(() => {
